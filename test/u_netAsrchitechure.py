e5d689dd973a317f5d691748e58c1fef6dd23aff
yanxiya <447592774@qq.com>
2020-08-25 10:21:53 +0800

    init partail CONV

--- test/u_netAsrchitechure.py
+++ test/u_netAsrchitechure.py
@@ -79,8 +79,8 @@ model = PConvUnet(vgg_weights='./data/logs/pytorch_to_keras_vgg16.h5')
 
 model.fit_generator(
     generator,
-    steps_per_epoch=2,
-    epochs=1,
+    steps_per_epoch=2000,
+    epochs=10,
     callbacks=[
         TensorBoard(
             log_dir='./data/logs/single_image_test',
